13289a0a3a7c3a61bf53884640a4ce957fba0c8e
Paul Bryan Reyes <75605942+paulreyesdigital@users.noreply.github.com>
2022-05-03 15:27:31 +0800

    Setup Tailwind Theme Colors
    Close #2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,21 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    colors: {
+      'primary-dark': '#2F3061',
+      'primary-light': '#90caf9',
+      'primary-light-hover': '#4badfe',
+      'secondary-light': '#FFE66D',
+      'neutral-dark': '#0A1929',
+      'neutral-light': '#F7FFF7',
+      'muted': '#ffffff1f',
+      'muted-text': 'rgba(255, 255, 255, 0.3)',
+      'container-color': '#001E3C',
+      ...colors
+    }
   },
   plugins: [],
 }
